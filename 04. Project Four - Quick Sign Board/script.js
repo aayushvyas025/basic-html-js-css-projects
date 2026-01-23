dc23656e11424fd0c349eacd9a1731d3a1073e99
Aayush Vyas <aayush25vyas@gmail.com>
2026-01-23 19:20:32 +0530

    Styling and Content Part Completed of Signature App

--- 04. Project Four - Quick Sign Board/script.js
+++ 04. Project Four - Quick Sign Board/script.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM Content Loaded Successfully !!!'); 
+
+    const colorPicker = document.getElementById('colorPicker'); 
+    const boardBackground = document.getElementById('BackgroundColor'); 
+    const penFontSize = document.getElementById('penSize');    
+    const clearBtn = document.getElementById('clearBtn'); 
+    const saveWithDownloadBtn = document.getElementById('saveWithDownloadBtn'); 
+    const previousStateBtn = document.getElementById('previousStateBtn');
+    const signBoard = document.getElementById('signBoard'); 
+
+    console.log("tools", colorPicker, boardBackground, penFontSize);
+    console.log("button", clearBtn, saveWithDownloadBtn, previousStateBtn); 
+    console.log("signBoard", signBoard);
+
     
 })
