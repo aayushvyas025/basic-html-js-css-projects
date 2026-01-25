21b763771fe3f0e93c11ea7dededa643e3f7fc99
Aayush Vyas <aayush25vyas@gmail.com>
2026-01-25 18:46:15 +0530

    Add the TypeEffect feature also, Project Complete

--- 05. Project Five - Portfolio Project First/script.js
+++ 05. Project Five - Portfolio Project First/script.js
@@ -2,9 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log(`DOM Content Loaded Successfully !!!`);
   const darkTheme = document.querySelector(".dark-theme");
   const application = document.querySelector("body");
+  new Typed("#dynamic-text", {
+    strings: ["DropXOut", "Youtuber", "Teacher", "Coder"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelayed: 1000,
+  });
+
   darkTheme.addEventListener("click", () => {
     console.log("Dark Theme Event Listener Working");
     application.classList.toggle("night");
-
   });
 });
