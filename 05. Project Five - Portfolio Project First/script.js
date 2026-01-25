5efe4be34810fd63a9e801d48d3581c1ef867dd4
Aayush Vyas <aayush25vyas@gmail.com>
2026-01-25 18:38:50 +0530

    Styling and toggle dark theme completed

--- 05. Project Five - Portfolio Project First/script.js
+++ 05. Project Five - Portfolio Project First/script.js
@@ -1,3 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log(`DOM Content Loaded Successfully !!!`);
-})
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(`DOM Content Loaded Successfully !!!`);
+  const darkTheme = document.querySelector(".dark-theme");
+  const application = document.querySelector("body");
+  darkTheme.addEventListener("click", () => {
+    console.log("Dark Theme Event Listener Working");
+    application.classList.toggle("night");
+
+  });
+});
